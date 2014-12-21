e4350a797b5146121de93b2f7cdba74fba708806
nomcycle <sean@rippleware.net>
2014-12-21 12:15:07 -0900

    Adding files for Jayson

---
+++ Assets/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public Transform[] waypoints;
+	public int current;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float distance = Vector3.Distance( waypoints[ current ].position, transform.position );
+		Vector2 direction = (waypoints [current].position - transform.position).normalized;
+		rigidbody2D.velocity = direction * 2;
+		if ( distance < 1 ) {
+			current++;
+		}
+	}
+}
---
+++ Assets/Tower.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tower : MonoBehaviour {
+
+	public Transform target;
+	public GameObject bullet;
+	public float fireRate;
+	private float fireTime;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.Find( "Enemy" ).transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		fireTime -= Time.deltaTime;
+
+		if ( fireTime < 0 ) {
+			GameObject newBullet = Instantiate( bullet ) as GameObject;
+			newBullet.transform.position = transform.position;
+			newBullet.rigidbody2D.velocity = (target.transform.position - transform.position).normalized * 5;
+			fireTime = fireRate;
+		}
+	}
+}
